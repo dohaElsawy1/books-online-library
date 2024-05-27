345f4993e4d3b0abb50ac42b1b37bf5b05cce34d
doha <doha.elsawy01@gmail.com>
2024-05-28 01:33:02 +0300

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/nav-bar/navbar/navbar.component';
+import { FavoritesComponent } from './components/favorites/favorites/favorites.component';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { HomeComponent } from './components/home/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FavoritesComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
